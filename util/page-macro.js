188d5378b55866b73478c16ea5549a53ad37c5b8
竹内佑介 <kaidouji85@gmail.com>
2020-11-07 17:38:18 +0900

    ページマクロにjsdocを追加した

--- util/page-macro.js
+++ util/page-macro.js
@@ -3,7 +3,16 @@ const browserPromise = chromium.launch({
   headless: false
 });
 
-module.exports.pageMacro = async function(t, callback) {
+/**
+ * ページオブジェクトを生成するマクロ
+ * 本関数は以下のplaywright公式サイトを参考に作成した
+ * https://playwright.dev/#version=v1.5.2&path=docs%2Ftest-runners.md&q=ava
+ *
+ * @param {ExecutionContext} t テストオブジェクト
+ * @param {(ExecutionContext, Page) => Promise<void>} callback
+ * @return {Promise<void>} テストが終了するまで待機するPromise
+ */
+async function pageMacro(t, callback) {
   const browser = await browserPromise;
   const page = await browser.newPage();
   try {
@@ -12,3 +21,5 @@ module.exports.pageMacro = async function(t, callback) {
     await page.close();
   }
 }
+
+module.exports.pageMacro = pageMacro;
