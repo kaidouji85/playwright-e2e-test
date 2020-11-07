7dc4a5cc76ff650d0abb87e3997808f0ff74fa2f
竹内佑介 <kaidouji85@gmail.com>
2020-11-07 12:39:23 +0900

    テストにアサーションを追加した

--- test.js
+++ test.js
@@ -1,25 +1,22 @@
 const { chromium } = require('playwright');
+const assert = require('assert');
 
 (async () => {
   const browser = await chromium.launch({
     headless: false
   });
   const context = await browser.newContext();
-
-  // Open new page
   const page = await context.newPage();
 
   // Go to https://www.wikipedia.org/
   await page.goto('https://www.wikipedia.org/');
-
-  // Click input[name="search"]
   await page.click('input[name="search"]');
-
-  // Fill input[name="search"]
   await page.fill('input[name="search"]', 'インターネット');
 
   // Go to https://ja.wikipedia.org/wiki/インターネット
   await page.goto('https://ja.wikipedia.org/wiki/インターネット');
+  const title = await page.textContent('[id="firstHeading"]');
+  assert.equal(title,'インターネット', 'タイトルが正しい');
 
   // Click //p[20]/a[1][normalize-space(.)='インターネット・プロトコル']
   await page.click('//p[20]/a[1][normalize-space(.)=\'インターネット・プロトコル\']');
