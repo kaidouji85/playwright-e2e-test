2da0659e16bd665c1f152f2edd1d7a44bd523075
竹内佑介 <kaidouji85@gmail.com>
2020-11-07 19:18:26 +0900

    スクリーンショットを追加した

--- test/main.test.js
+++ test/main.test.js
@@ -4,16 +4,19 @@ const test = require('ava').default;
 test('一通り検索、ページ閲覧ができる', pageMacro, async (t, page) => {
   // Go to https://www.wikipedia.org/
   await page.goto('https://www.wikipedia.org/');
+  await page.screenshot({path: 'screen-shots/top-page.png'});
   await page.click('input[name="search"]');
   await page.fill('input[name="search"]', 'インターネット');
 
   // Go to https://ja.wikipedia.org/wiki/インターネット
   await page.goto('https://ja.wikipedia.org/wiki/インターネット');
+  await page.screenshot({path: 'screen-shots/internet.png'});
   const title = await page.textContent('[id="firstHeading"]');
   t.is(title, 'インターネット', 'タイトルが正しい');
 
   // Click //p[20]/a[1][normalize-space(.)='インターネット・プロトコル']
   await page.click('//p[20]/a[1][normalize-space(.)=\'インターネット・プロトコル\']');
+  await page.screenshot({path: 'screen-shots/internet-protocol.png'});
   // assert.equal(page.url(), 'https://ja.wikipedia.org/wiki/Internet_Protocol');
 
   // Close page
