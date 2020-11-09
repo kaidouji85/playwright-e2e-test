17f40c0bfd13e22f4079d53e4e928d6a34f8d88a
竹内佑介 <kaidouji85@gmail.com>
2020-11-09 21:48:10 +0900

    ページ全体のスクリーンショットを取得するようにした

--- test/main.test.js
+++ test/main.test.js
@@ -3,16 +3,16 @@ const test = require('ava').default;
 
 test('一通り検索、ページ閲覧ができる', pageMacro, async (t, page) => {
   await page.goto('https://www.wikipedia.org/');
-  await page.screenshot({path: 'screen-shots/top-page.png'});
+  await page.screenshot({path: 'screen-shots/top-page.png', fullPage: true});
   await page.fill('input[name="search"]', 'インターネット');
   await page.click('//button[normalize-space(.)=\'Search\']');
 
-  await page.screenshot({path: 'screen-shots/internet.png'});
   const title = await page.textContent('[id="firstHeading"]');
   t.is(title, 'インターネット', 'タイトルが正しい');
+  await page.screenshot({path: 'screen-shots/internet.png', fullPage: true});
 
   await page.click('//p[20]/a[1][normalize-space(.)=\'インターネット・プロトコル\']');
-  await page.screenshot({path: 'screen-shots/internet-protocol.png'});
+  await page.screenshot({path: 'screen-shots/internet-protocol.png', fullPage: true});
 
   await page.close();
 });
